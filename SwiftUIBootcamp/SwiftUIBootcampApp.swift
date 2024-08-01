4f8b028d0bbeb5a14e0e14a7bac988d9fd708e8a
Eren-Unal <bilalunal24@gmail.com>
2024-08-01 09:48:10 +0300

    Initial Commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Eren on 1.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
